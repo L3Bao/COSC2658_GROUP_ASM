1857a67e8f72289da661932176f6ecd8da7697d7
L3Bao <118871111+L3Bao@users.noreply.github.com>
2024-04-17 11:31:17 +0700

    revised the arrayList and the place class

--- place/Place.java
+++ place/Place.java
@@ -1,20 +1,22 @@
 package place;
+import arrayList.ArrayList; // Correct import of your custom ArrayList class
 
 public class Place {
     double x;
     double y;
-    String serviceType;
+    ArrayList<String> serviceTypes;
 
     public Place() {
         this.x = 0.0;
         this.y = 0.0;
-        this.serviceType = "";
+        // this.serviceTypes = new ArrayList<String>();
     }
 
     public Place(double x, double y, String serviceType) {
         this.x = x;
         this.y = y;
-        this.serviceType = serviceType;
+        this.serviceTypes = new ArrayList<String>();
+        this.serviceTypes.insertAt(0, serviceType); // Correct use of insertAt
     }
 
     public double getX() {
@@ -33,27 +35,39 @@ public class Place {
         this.y = y;
     }
 
-    public String getServiceType() {
-        return this.serviceType;
+    public ArrayList<String> getServiceTypes() {
+        return this.serviceTypes;
     }
 
-    public void setServiceType(String serviceType) {
-        this.serviceType = serviceType;
+    public void addServiceType(String serviceType) {
+        this.serviceTypes.insertAt(this.serviceTypes.size(), serviceType); // Insert at the end
     }
 
-    public void updateServiceType(String serviceType) {
-        this.setServiceType(serviceType);
-        System.out.println("New Service Type: " + serviceType);
+    public void updateServiceType(int index, String serviceType) {
+        if (index >= 0 && index < this.serviceTypes.size()) {
+            this.serviceTypes.replaceAt(index, serviceType); // Use replaceAt
+            System.out.println("Service Type Updated: " + serviceType);
+        } else {
+            System.out.println("Invalid index for service types.");
+        }
     }
 
     public String toString() {
-        return "Place{x=" + this.x + ", y=" + this.y + ", serviceType='" + this.serviceType + "'}";
+        StringBuilder result = new StringBuilder("Place{x=" + this.x + ", y=" + this.y + ", serviceTypes=[");
+        for (int i = 0; i < serviceTypes.size(); i++) {
+            result.append(serviceTypes.get(i));
+            if (i < serviceTypes.size() - 1) result.append(", ");
+        }
+        result.append("]}");
+        return result.toString();
     }
 
     public static void main(String[] args) {
         Place place1 = new Place(1.0, 1.0, "Donut shop");
-        System.out.println(place1.toString());
-        place1.updateServiceType("Pee");
-        System.out.println(place1.toString());
+        System.out.println(place1);
+        place1.addServiceType("Coffee shop");
+        System.out.println(place1);
+        place1.updateServiceType(1, "Bakery");
+        System.out.println(place1);
     }
 }
---
+++ place/tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+        // this.serviceTypes = new ArrayList<String>();
